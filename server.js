160e4df8ab5ba8e3509e737b11b46b683c47aa91
Jeeva Nadarajah <jeeva.ganeshan@gmail.com>
2018-01-22 21:31:04 -0500

    got the server starting without errors

--- server.js
+++ server.js
@@ -6,12 +6,38 @@
 // call the packages we need
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
+var mongoose = require('mongoose');
+var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var config = require('config');
+
+var book = require('./controllers/routes/book');
+
+//db options
+let options = {
+                server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } }
+              };
+
+//db connection
+mongoose.connect(config.DBHost, options);
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+//don't show the log when it is test
+if(config.util.getEnv('NODE_ENV') !== 'test') {
+    //use morgan to log at command line
+    app.use(morgan('combined')); //'combined' outputs the Apache style LOGs
+}
+
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/json'}));
+
 
 var port = process.env.PORT || 8080;        // set our port
 
@@ -24,14 +50,22 @@ router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });
 });
 
+
 // more routes for our API will happen here
+app.route("/book")
+    .get(book.getBooks)
+    .post(book.postBook);
+app.route("/book/:id")
+    .get(book.getBook)
+    .delete(book.deleteBook)
+    .put(book.updateBook);
+
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);
 
 app.get('/', function(req, res) {
-  console.log(__dirname + '/index.html');
   res.sendfile(__dirname + '/index.html')
 });
 
@@ -39,3 +73,5 @@ app.get('/', function(req, res) {
 // =============================================================================
 app.listen(port);
 console.log('Magic happens on port ' + port);
+
+module.exports = app; // for testing
